d986c36360ecc5cd940cffe658be1e06a442e260
Renata Betugan <103539839+Reneddy@users.noreply.github.com>
2024-02-28 01:52:30 +0300

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,17 @@
-"use strict"
+"use strict";
+//Задача 1 Решение квадратного уравнения
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let roots = [];
+	let d = Math.pow(b, 2) - 4 * a * c;
+	return d > 0 ? (roots.push((-b + Math.sqrt(d)) / (2 * a)), roots.push((-b - Math.sqrt(d)) / (2 * a))) : d === 0 ? (roots.push(-b / (2 * a))) : [], roots;
 }
+console.log(solveEquation(a, b, c));
 
+
+//Задача 2 Калькулятор ипотеки
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	let payment = (amount - contribution) * (percent / 1200 + ((percent / 1200)) / ((Math.pow((1 + percent / 1200), countMonths)) - 1));
+	let creditAmount = payment * countMonths;
+	return creditAmount.toFixed(2);
 }
+console.log(calculateTotalMortgage(percent, contribution, amount, countMonths));
