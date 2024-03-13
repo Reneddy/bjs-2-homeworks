2593a103c62177e6b2b9a2937215d32602b76f87
Renata Betugan <103539839+Reneddy@users.noreply.github.com>
2024-03-14 02:11:07 +0300

    Update task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,22 @@
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 function getUsersNamesInAgeRange(users, gender) {
+  let filteredUsers = users.filter(user => user.gender === gender);
+  if (filteredUsers.length===0) {
+  return 0;
+  }
+  let totalAge = filteredUsers.reduce((acc, user) => acc + user.age, 0);
+  let averageAge = totalAge / filteredUsers.length;
+  return averageAge;
   
 }
