13af03f302e9db21bf4e8c1963f5c386c7bd4ca4
Reneddy <beturen@gmail.com>
2024-03-15 00:00:04 +0300

    updated

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,20 +1,32 @@
 function Student(name, gender, age) {
-
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if (!this.marks) {
+     console.log('Error: student has been expelled');
+     return;
+     }
+    this.marks.push(...marks);
 }
 
 Student.prototype.getAverage = function () {
-  
+  if (!this.marks||!this.marks.length) {
+    return 0;
+  }
+  let sum = this.marks.reduce((acc, curr) => acc + curr);
+  return sum/this.marks.length;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
 }
